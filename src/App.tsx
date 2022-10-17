75da6229ddd1139b9c7d362850cbcf67b707e4ec
Liu680 <huya@lingqi69.top>
2022-10-17 20:01:30 +0800

    liu680/liu680-web

---
+++ src/App.tsx
@@ -0,0 +1,101 @@
+import "./App.css";
+
+import intl from "react-intl-universal";
+import React, { useState, useEffect } from "react";
+import { Layout, Row, Col, Spin } from "antd";
+import ServerRow from "./ServerRow";
+
+require("intl/locale-data/jsonp/en.js");
+require("intl/locale-data/jsonp/zh.js");
+
+const { Header, Footer, Content } = Layout;
+
+const App: React.FC = () => {
+  const [serverData, setServerData] = useState({ servers: [], updated: "0" });
+  const [isOnline, setIsOnline] = useState(false);
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("json/stats.json")
+        .then((res) => res.json())
+        .then((data) => {
+          setServerData(data);
+          setIsOnline(true);
+        })
+        .catch((e) => console.log("错误:", e));
+    };
+    fetchData();
+    let itv = setInterval(fetchData, 1000);
+    return () => {
+      clearInterval(itv);
+    };
+  }, []);
+
+  const [initDone, setInitDone] = useState(false);
+  let currentLocale = navigator.language || "zh-CN";
+  if (currentLocale === "zh-TW" || currentLocale === "zh-HK") {
+    currentLocale = "zh-TW";
+  } else if (currentLocale.startsWith("zh")) {
+    currentLocale = "zh-CN";
+  } else {
+    currentLocale = "en-US";
+  }
+
+  initDone ||
+    fetch(`locales/${currentLocale}.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        return intl.init({
+          currentLocale,
+          locales: {
+            [currentLocale]: data,
+          },
+        });
+      })
+      .then(() => {
+        setInitDone(true);
+      });
+
+  return (
+    <div className="App">
+      <Layout>
+        <Header>
+          <div className="logo">ServerStatus</div>
+        </Header>
+        <Content style={{ background: "#fff" }}>
+          <Row justify="center">
+            <Col xs={24} sm={23} md={23} lg={22} xl={20} xxl={16}>
+              {initDone ? (
+                <Spin size="large" spinning={!isOnline} tip="Loading...">
+                  <ServerRow {...serverData} />
+                </Spin>
+              ) : (
+                  <div />
+                )}
+            </Col>
+          </Row>
+        </Content>
+        <Footer className="footer">
+          <a target="_blank" href="/detail">🗂️</a>
+          {" | "}
+          <a
+            href="https://github.com/krwu/ServerStatus-web"
+            rel="external noopener"
+          >
+            WebUI
+          </a>{" "}
+          for{" "}
+          <a
+            href="https://github.com/zdz/ServerStatus-Rust"
+            rel="external noopener"
+          >
+            ServerStatus-Rust 版
+          </a>
+          {" | "}
+          <a target="_blank" href="/map">🗺️</a>
+        </Footer>
+      </Layout>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/ServerRow.tsx
@@ -0,0 +1,185 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+import intl from 'react-intl-universal';
+import React, { ReactNode } from 'react';
+import {
+  Row, Col, Progress, Tooltip, Alert,
+} from 'antd';
+import {
+  CheckCircleFilled,
+  WarningFilled
+} from '@ant-design/icons';
+
+interface RawData {
+  name: string;
+  type: string;
+  alias: string;
+  location: string;
+  online4: boolean;
+  online6: boolean;
+  uptime: string;
+  load_1: number;
+  load_5: number;
+  load_15: number;
+  'network_rx': number;
+  'network_tx': number;
+  'network_in': number;
+  'network_out': number;
+
+  cpu: number;
+  memory_total: number;
+  memory_used: number;
+  'swap_total': number;
+  'swap_used': number;
+  'hdd_total': number;
+  'hdd_used': number;
+  custom?: string;
+}
+
+interface SergateData {
+  servers?: Array<RawData>;
+  updated?: string;
+}
+
+function onlineTag(online: boolean, label: string): React.ReactElement {
+  //return <Tag color={online ? '#87d068' : '#f50'}>{online ? 'O N' : 'OFF'}</Tag>;
+  return online ? <CheckCircleFilled /> : <WarningFilled />;
+}
+
+
+function networkUnit(network: number): string {
+  network = network || 0;
+  if (network < 1000) {
+    return `${network.toFixed(0)}B`;
+  } if (network < 1000 * 1000) {
+    return `${(network / 1000).toFixed(0)}K`;
+  }
+  return `${(network / 1000 / 1000).toFixed(0)}M`;
+}
+
+function bytesToSize(bytes: number, precision: number = 1, si: number = 0) {
+  const kilobyte = si === 0 ? 1024 : 1000;
+  const megabyte = kilobyte * kilobyte;
+  const gigabyte = megabyte * kilobyte;
+  const terabyte = gigabyte * kilobyte;
+  bytes = bytes || 0;
+  bytes *= 1024;
+  if (bytes >= 0 && bytes < kilobyte) {
+    return `${bytes}B`;
+  }
+
+  if (bytes >= kilobyte && bytes < megabyte) {
+    return `${(bytes / kilobyte).toFixed(precision)}K`;
+  }
+
+  if (bytes >= megabyte && bytes < gigabyte) {
+    return `${(bytes / megabyte).toFixed(precision)}M`;
+  }
+
+  if (bytes >= gigabyte && bytes < terabyte) {
+    return `${(bytes / gigabyte).toFixed(precision)}G`;
+  }
+
+  return `${(bytes / terabyte).toFixed(precision)}T`;
+}
+
+function memTips(props: RawData): ReactNode {
+  const {
+    memory_used, memory_total, swap_used, swap_total,
+  } = props;
+  return (
+    <dl>
+      <dt>Mem:</dt>
+      <dd>
+        {bytesToSize(memory_used)}
+        /
+        {bytesToSize(memory_total)}
+      </dd>
+      <dt>Swap:</dt>
+      <dd>
+        {bytesToSize(swap_used)}
+        /
+        {bytesToSize(swap_total)}
+      </dd>
+    </dl>
+  );
+}
+
+function formatDateTime(time: Date) {
+  const year = time.getFullYear();
+  const month = (`${time.getMonth() + 1}`).padStart(2, '0');
+  const day = (`${time.getDate()}`).padStart(2, '0');
+  const hour = (`${time.getHours()}`).padStart(2, '0');
+  const minute = (`${time.getMinutes()}`).padStart(2, '0');
+  const second = (`${time.getSeconds()}`).padStart(2, '0');
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+}
+
+const ServerRow: React.FC<SergateData> = (props: SergateData) => {
+  let { servers, updated } = props;
+
+  servers = servers || [];
+  updated = updated || '0';
+  const updatedInt = parseInt(updated, 10) * 1000;
+  const updatedTime = formatDateTime(new Date(updatedInt));
+  let idx = 1;
+
+  return (
+    <div className="sergate">
+      <Row className="sr-head" justify="space-around" gutter={10}>
+        <Col xs={3} sm={3} md={1} lg={1}>IPv4</Col>
+        <Col xs={0} sm={0} md={1} lg={1}>IPv6</Col>
+        <Col xs={5} sm={4} md={2} lg={2}>{intl.get('NAME')}</Col>
+        <Col xs={0} sm={2} md={2} lg={2}>{intl.get('TYPE')}</Col>
+        <Col xs={2} sm={2} md={1} lg={1}>{intl.get('LOC')}</Col>
+        <Col xs={4} sm={4} md={3} lg={2}>{intl.get('UPTIME')}</Col>
+        <Col xs={0} sm={0} md={0} lg={1}>{intl.get('LOAD')}</Col>
+        <Col xs={0} sm={0} md={5} lg={4}>{intl.get('NETWORK')}</Col>
+        <Col xs={3} sm={3} md={3} lg={3}>{intl.get('CPU')}</Col>
+        <Col xs={3} sm={3} md={3} lg={3}>{intl.get('RAM')}</Col>
+        <Col xs={4} sm={3} md={3} lg={3}>{intl.get('HDD')}</Col>
+      </Row>
+      {servers && servers.length > 0 ? servers.map((server) => (
+        <Row key={server.name} className="sr-body" justify="space-around" gutter={10}>
+          <span className="col-num">{idx++}</span>
+          <Col xs={3} sm={3} md={1} lg={1}>{onlineTag(server.online4, 'IPv4')}</Col>
+          <Col xs={0} sm={0} md={1} lg={1}>{onlineTag(server.online6, 'IPv6')}</Col>
+          <Col xs={5} sm={4} md={2} lg={2}>{server.alias || server.name}</Col>
+          <Col xs={0} sm={2} md={2} lg={2}>{server.type}</Col>
+          <Col xs={2} sm={2} md={1} lg={1}>{server.location}</Col>
+          <Col xs={4} sm={4} md={3} lg={2}>{server.uptime}</Col>
+          <Col xs={0} sm={0} md={0} lg={1}>{server.load_1}</Col>
+          <Col xs={0} sm={0} md={5} lg={4}>
+            {networkUnit(server.network_rx)}
+            ↓ | ↑
+            {networkUnit(server.network_tx)}
+          </Col>
+          <Col xs={3} sm={3} md={3} lg={3}>
+            <Progress className="sg-progress" strokeLinecap="square" strokeWidth={12} percent={server.cpu} status="active" />
+          </Col>
+          <Col xs={3} sm={3} md={3} lg={3}>
+            <Tooltip placement="left" title={memTips(server)}>
+              <Progress className="sg-progress" strokeLinecap="square" strokeWidth={12} percent={parseFloat(((server.memory_used / server.memory_total) * 100).toFixed(1))} status="active" />
+            </Tooltip>
+          </Col>
+          <Col xs={4} sm={3} md={3} lg={3}>
+            <Tooltip placement="left" title={`${bytesToSize(server.hdd_used * 1024)}/${bytesToSize(server.hdd_total * 1024)}`}>
+              <Progress className="sg-progress" strokeLinecap="square" strokeWidth={12} percent={parseFloat(((server.hdd_used / server.hdd_total) * 100).toFixed(1))} status="active" />
+            </Tooltip>
+          </Col>
+        </Row>
+      )) : (
+        <Alert
+          showIcon
+          type="info"
+          message={intl.get('LOADING')}
+          description={intl.get('WAIT')}
+        />
+      )}
+      {updatedInt > 0 && <Alert className="lastUpdated" type="info" message={intl.get('LAST_UPDATE', { updatedTime })} />}
+    </div>
+  );
+};
+
+export default ServerRow;
